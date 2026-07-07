f2a7ce3637d0690cbad1398546a64d33a6ba43dd
Jahanzaib <jahanzaibashraf1318@gmail.com>
2026-07-07 23:09:06 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="mirage-honeypot",
     version="0.1.0",
-    description="A lightweight Python honeypot for observing basic network probes",
+    description="A lightweight Python honeypot for observing attacker behaviours",
+    author="Jahanzaib Ashraf Mir",
     packages=find_packages(),
     py_modules=["run"],
     entry_points={"console_scripts": ["mirage=run:main"]},
